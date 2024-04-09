17a77776dc8b0cd78df8dc9aa2f7ac7f71701d9d
HarshaReddy514 <devilharsha514@gmail.com>
2024-04-09 23:03:37 +0530

    First Commit

---
+++ src/main/java/com/niq/activate/ActivateApplication.java
@@ -0,0 +1,13 @@
+package com.niq.activate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivateApplication.class, args);
+	}
+
+}
