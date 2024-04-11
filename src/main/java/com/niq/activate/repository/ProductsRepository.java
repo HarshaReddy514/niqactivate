b69e2b6b573502cba6477e423998cecaf5d6f8c9
HarshaReddy514 <devilharsha514@gmail.com>
2024-04-11 14:46:35 +0530

    Added category and brand filter to fetch products

--- src/main/java/com/niq/activate/repository/ProductsRepository.java
+++ src/main/java/com/niq/activate/repository/ProductsRepository.java
@@ -1,12 +1,16 @@
 package com.niq.activate.repository;
 
 import com.niq.activate.entities.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Product, String> {
 
     Product findProductsByProductId(String productId);
 
+    List<Product> findProductByCategoryAndBrand(String category, String brand, Pageable pageable);
 }
--- src/main/java/com/niq/activate/service/ProductServiceImpl.java
+++ src/main/java/com/niq/activate/service/ProductServiceImpl.java
@@ -9,11 +9,13 @@ import com.niq.activate.repository.ShoppersRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -39,11 +41,18 @@ public class ProductServiceImpl implements ProductService{
     @Cacheable("filteredProducts")
     public List<Product> getProductsByFilters(String shopperId, String category, String brand, int limit) {
         Optional<Shopper> shopper = shoppersRepository.findByShopperId(shopperId);
+        List<Product> finalProducts = new ArrayList<>();
+        if(category != null || brand != null){
+            Pageable pageable = PageRequest.of(0, limit);
+            finalProducts = productsRepository.findProductByCategoryAndBrand(category, brand, pageable);
+        }
         if(shopper.isEmpty()){
             throw new ShopperNotFoundException("Shopper with shopper id : " + shopperId + " is not found");
         } else {
             List<Shelf> shelf = shopper.get().getShelf();
-            return shelf.stream().map(item -> productsRepository.findProductsByProductId(item.getProductId())).collect(Collectors.toList());
+            finalProducts.addAll(shelf.stream().map(item -> productsRepository.findProductsByProductId(item.getProductId())).toList());
         }
+
+        return finalProducts;
     }
 }
